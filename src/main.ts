0bf5022e3a4cf52504742d58975594ebe0b2f22b
parthodave <parthodave@gmail.com>
2024-12-27 23:08:03 +0530

    Interceptor resolved.

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { LoginComponent } from './app/login/login.component';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { AppComponent } from './app/app.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor } from './app/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
-    { 
-      provide: HTTP_INTERCEPTORS, 
-      useClass: AuthInterceptor, 
-      multi: true 
-    },
-     provideRouter(routes) // Use 'routes' here
+    provideHttpClient(
+      withInterceptors([AuthInterceptor])
+    ),
+    provideRouter(routes)
   ]
-
-
 }).catch(err => console.error(err));
