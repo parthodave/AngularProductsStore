78808e407eef790e99d55f39de0554962a1cb0d8
parthodave <parthodave@gmail.com>
2025-01-11 13:05:30 +0530

    Correct Book ts file.

--- src/app/Books/books-list/books-list.component.ts
+++ src/app/Books/books-list/books-list.component.ts
@@ -1,9 +1,9 @@
-import { Books } from '../../Models/Books';
 import { Component } from '@angular/core';
 import { User } from '../../Models/User';
 import { AuthServiceService } from '../../Service/auth.service';
 import { CommonModule } from '@angular/common';
 import { BookService } from '../../Service/book.service';
+import { Book } from '../../Models/Book';
 
 @Component({
   selector: 'app-books-list',
@@ -13,7 +13,7 @@ import { BookService } from '../../Service/book.service';
   styleUrl: './books-list.component.css'
 })
 export class BooksListComponent {
-Books : Books[] = [];
+Books : Book[] = [];
 
 constructor(private authService : BookService ){
 
--- src/app/Service/book.service.ts
+++ src/app/Service/book.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from '../Models/Book';
 
+
 @Injectable({
   providedIn: 'root'
 })
