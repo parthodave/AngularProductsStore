f76c386e8ad0b33ae33c6a52259f764214eabe7f
parthodave <parthodave@gmail.com>
2025-01-17 15:17:42 +0530

    Filter added to bookslist.

--- src/app/Books/books-list/books-list.component.ts
+++ src/app/Books/books-list/books-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { User } from '../../Models/User';
 import { AuthServiceService } from '../../Service/auth.service';
 import { CommonModule } from '@angular/common';
@@ -8,12 +9,13 @@ import { Book } from '../../Models/Book';
 @Component({
   selector: 'app-books-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './books-list.component.html',
   styleUrl: './books-list.component.css'
 })
 export class BooksListComponent {
 Books : Book[] = [];
+searchText : string = '';
 
 constructor(private authService : BookService ){
 
@@ -31,5 +33,20 @@ ngOnInit(){
   });
 }
 
+SearchBooks() {
+  this.authService.GetBooks().subscribe({
+    next: (booklist) => {
+      this.Books = booklist;
+      this.Books = this.Books.filter((book) => (
+          book.BookName.toLowerCase().includes(this.searchText.toLowerCase()) || 
+          book.BookAuthor.toLowerCase().includes(this.searchText.toLowerCase())
+        )
+      );
+    },
+    error: (err) => {
+      console.log(err);
+    }
+  });
+}
 
 }
